bd13a30906da9a8fbaf34659f77f0baa8814c923
Franz <fmhorfilla.wbridges@gmail.com>
2024-03-06 13:20:25 +0800

    update profile

--- assets/js/main.js
+++ assets/js/main.js
@@ -218,4 +218,9 @@
       });
   });
 
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+
 })()
